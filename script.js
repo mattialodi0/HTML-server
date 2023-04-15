bc8f4317ed6baea9e2ff9a784819bd357bfef873
mattyk0207 <101145004+mattyk0207@users.noreply.github.com>
2023-04-15 17:56:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,9 +34,10 @@ document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
 });
 
-
 document.querySelector(".search-bar").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
         weather.search();
     }
 });
+
+weather.search("Rome");
