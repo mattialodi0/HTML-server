1270a42c285fcd16c73f585d9f13a766a0c0b058
mattyk0207 <101145004+mattyk0207@users.noreply.github.com>
2023-04-15 17:31:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-let prices = [5, 10.5, 9.6, 6];
-let tot = 0;
+let weather = {
+    appKey: "b32258fedb5b2ecc68819748ab89e935",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city +
+            "&units=metric&appid="
+            + this.appKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+}
 
-const submit_btn = document.querySelector("#submit");
-const orders_list = document.querySelector("#orders");
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
 
-function updateTotal() {
-    for(let i=0; i < prices.length; i++) {
-        if (document.getElementsByClassName("type")[i]) {
-            let type_num = (document.getElementsByClassName("type")[i].value);
-            tot += type_num * prices[i];
-        }
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
     }
-    tot.toPrecision(2);     //non funziona
-    document.getElementById("total-price").innerHTML = "Il totale del tuo ordine è di: " + tot + "€";
-}
-
-const takeOrder = e => {
-    e.preventDefault();
-
-    let li = document.createElement('li');
-    li.textContent =  tot;
-    localStorage.setItem('order', JSON.stringify(tot));
-    orders_list.appendChild(li);
-    tot = 0;
-    document.getElementById("total-price").innerHTML = "Il totale del tuo ordine è di: " + tot + "€";
-}
-
-
-
-
-
-submit_btn.addEventListener("click", takeOrder);
-
-
+});
