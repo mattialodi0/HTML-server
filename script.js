006a88827706a8e43f9d69e180284fe3ac9a4322
mattyk0207 <101145004+mattyk0207@users.noreply.github.com>
2023-09-21 22:07:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,24 @@
-feather.replace();
 
-const controls = document.querySelector('.controls');
-const video = document.querySelector('video');
-const canvas = document.querySelector('canvas');
-const screenshotImage = document.querySelector('img');
-const buttons = [...controls.querySelectorAll('button')];
-let streamStarted = false;
+var video, canvas, ctx;
+if(navigator && navigator.mediaDevices){
+    const options = { audio: false, video: { facingMode: "user", width: 300, height: 300  } }
+    navigator.mediaDevices.getUserMedia(options)
+    .then(function(stream) {
+        video = document.querySelector('video');
+        video.srcObject = stream;
+        video.onloadedmetadata = function(e) {
+          video.play();
+        };
+        canvas = document.getElementById("myCanvas");
+        ctx = canvas.getContext('2d');
+    })
+    .catch(function(err) {
+        //Handle error here
+    });
+}else{
+    console.log("camera API is not supported by your browser")
+}
 
-const [play, pause, screenshot] = buttons;
-
-const constraints = {
-  video: {
-    width: {
-      min: 1280,
-      ideal: 1920,
-      max: 2560,
-    },
-    height: {
-      min: 720,
-      ideal: 1080,
-      max: 1440
-    },
-    facingMode: { exact: "environment" }
-  }
-};
-
-startStream(constraints);
-
-play.onclick = () => {
-  if (streamStarted) {
-    video.play();
-    play.classList.add('d-none');
-    pause.classList.remove('d-none');
-    return;
-  }
-  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-    const updatedConstraints = {
-      ...constraints,
-      deviceId: {
-        exact: cameraOptions.value
-      }
-    };
-    startStream(updatedConstraints);
-  }
-};
-
-const pauseStream = () => {
-  video.pause();
-  play.classList.remove('d-none');
-  pause.classList.add('d-none');
-};
-
-const doScreenshot = () => {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.getContext('2d').drawImage(video, 0, 0);
-  screenshotImage.src = canvas.toDataURL('image/webp');
-  screenshotImage.classList.remove('d-none');
-};
-
-pause.addEventListener('click', pauseStream);
-screenshot.addEventListener('click', doScreenshot);
-
-const startStream = async (constraints) => {
-  const stream = await navigator.mediaDevices.getUserMedia(constraints);
-  handleStream(stream);
-};
-
-
-const handleStream = (stream) => {
-  video.srcObject = stream;
-  play.classList.add('d-none');
-  pause.classList.remove('d-none');
-  screenshot.classList.remove('d-none');
-
-};
+function clickPhoto() {
+   ctx.drawImage(video, 0,0, canvas.width, canvas.height);
+ }
